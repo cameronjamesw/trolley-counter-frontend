a8c231aa8a3f11db9cdb5c2e949ec13f0ddb28c5
Cameron <cameron_2000@live.co.uk>
2025-05-31 19:34:04 +0100

    add further styling to navbar, compiled bootstrap

--- trolley-counter/src/components/NavBar.js
+++ trolley-counter/src/components/NavBar.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Navbar, Container, NavLink } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar fixed="top" className={styles.NavBar}>
+    <Navbar
+    data-bs-theme="dark"
+    expand="lg" 
+    className={styles.NavBar}>
       <Container>
-        <NavLink className={styles.NavLink}>
-          <Navbar.Brand>Trolley Counter</Navbar.Brand>
-        </NavLink>
+        <Navbar.Brand href="#home">Trolley Counter</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Stats</Nav.Link>
+            <NavDropdown title="Trollies" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Front Labels</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Back Labels
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Missing Labels</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
