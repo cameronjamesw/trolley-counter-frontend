f976e2b812537b3e7afd5c010336a5ef4f503865
Cameron <cameron_2000@live.co.uk>
2025-06-02 11:34:38 +0100

    add further fields to sign up form

--- trolley-counter/src/auth/SignUpForm.js
+++ trolley-counter/src/auth/SignUpForm.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const SignUpForm = () => {
   return (
     <Form>
-        <h1>Sign Up</h1>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <h1>Sign Up</h1>
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label className="d-none">Username</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter username"
+          name="username"
+        />
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label className="d-none">Email address</Form.Label>
+        <Form.Control 
+        type="email" 
+        placeholder="Enter email" 
+        name="email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password1">
+        <Form.Label className="d-none">Password</Form.Label>
+        <Form.Control 
+        type="password" 
+        placeholder="Password" 
+        name="password1" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password2">
+        <Form.Label className="d-none">Confirm Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Confirm Password"
+          name="password2"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
@@ -24,7 +42,7 @@ const SignUpForm = () => {
         Submit
       </Button>
     </Form>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
