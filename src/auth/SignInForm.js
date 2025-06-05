547ea3212168a42e0ec3c716294c6b4e1c5fe4ea
Cameron <cameron_2000@live.co.uk>
2025-06-05 11:35:40 +0100

    add submit functionality to sign in form, added error fields

--- src/auth/SignInForm.js
+++ src/auth/SignInForm.js
@@ -1,19 +1,66 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
+import axios from "../api/axiosDefaults";
+import React, { useState } from 'react'
+import { Alert, Button, Form } from 'react-bootstrap'
 
 const SignInForm = () => {
-  return (
-    <Form>
-        <h1 className='text-white my-4'>Sign In Form</h1>
-      <Form.Group className="mt-4" controlId="formBasicEmail">
-        <Form.Label className='d-none'>Username</Form.Label>
-        <Form.Control type="email" placeholder="Enter username" />
-      </Form.Group>
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: ""
+  })
 
-      <Form.Group className="mt-4" controlId="formBasicPassword">
-        <Form.Label className='d-none'>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+  const {username, password} = signInData
+  const [errors, setErrors] = useState({})
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name] : event.target.value
+    })
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const {data} = await axios.post('/dj-rest-auth/login/', signInData);
+    } catch (err) {
+      setErrors(err.response?.data)
+    }
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+        <h1 className='text-white my-4'>Sign In Form</h1>
+      <Form.Group className="mt-4" controlId="username">
+        <Form.Label className='d-none'>Username</Form.Label>
+        <Form.Control 
+        type="text" 
+        placeholder="Enter username"
+        name="username"
+        value={username}
+        onChange={handleChange} />
       </Form.Group>
+      {Array.isArray(errors?.username) &&
+        errors.username.map((msg, idx) => (
+          <Alert variant="danger" key={idx}>
+            {msg}
+          </Alert>
+        ))}
+
+      <Form.Group className="mt-4" controlId="password">
+        <Form.Label className='d-none'>Password</Form.Label>
+        <Form.Control 
+        type="password" 
+        placeholder="Password"
+        name="password"
+        value={password}
+        onChange={handleChange} />
+      </Form.Group>
+      {Array.isArray(errors?.password) &&
+      errors.password.map((idx, msg) => {
+        <Alert key={idx} variant="danger">
+          {msg}
+        </Alert>
+      })}
 
       <Button variant="success" className='mt-4' type="submit">
         Submit
