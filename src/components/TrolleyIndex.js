7345e3ad0d571eea1c3d8b2ffd0dd11fa0ae1a6d
Cameron <cameron_2000@live.co.uk>
2025-06-09 20:33:58 +0100

    added styling to add trolley button

--- src/components/TrolleyIndex.js
+++ src/components/TrolleyIndex.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Row } from 'react-bootstrap'
 import styles from '../styles/TrolleyIndex.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const TrolleyIndex = () => {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  }
+
+  const buttonStyle = {
+    backgroundColor: isHovered ? '#006400' : '#2d843d',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+  };
+  
   return (
-    <Container className={`mt-4 ${styles.Main}`}>
-        <p className='p-3'>This is the trolley index</p>
+    <Row>
+    <Container 
+    style={buttonStyle}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    className={`d-flex col-2 col-lg-1 mt-4 ${styles.Main} ${styles.IconBox}`}>
+      <FontAwesomeIcon className={styles.Icon} icon={faCartShopping} />
     </Container>
+    <Container className={`col-8 col-lg-8 mt-4 ${styles.Main}`}>
+        <p className='p-3'>Trolley Index</p>
+    </Container>
+    </Row>
   )
 }
 
