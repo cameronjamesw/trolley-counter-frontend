3ab359c20c478c46c78a9ca29ef51ba9b054eaaf
Cameron <cameron_2000@live.co.uk>
2025-06-10 11:47:28 +0100

    added basic form to addTrolleyForm.js

--- src/pages/trollies/AddTrolleyForm.js
+++ src/pages/trollies/AddTrolleyForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { Form } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const AddTrolleyForm = () => {
   const currentUser = useCurrentUser();
@@ -10,7 +12,29 @@ const AddTrolleyForm = () => {
   }
 
   useRedirect(!currentUser ? "loggedIn" : "loggedOut");
-  return <div>This is the add trolley form</div>;
+
+  return (
+    <Form onSubmit={() => {}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default AddTrolleyForm;
