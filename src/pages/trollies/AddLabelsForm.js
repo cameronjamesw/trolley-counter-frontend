6b5ebeb6fa9cc93d6bce336fd4a1cfbd8c780dc6
Cameron <cameron_2000@live.co.uk>
2025-06-12 14:00:05 +0100

    add click functionality

--- src/pages/trollies/AddLabelsForm.js
+++ src/pages/trollies/AddLabelsForm.js
@@ -3,69 +3,55 @@ import { Form, Container, Row, Col } from "react-bootstrap";
 import styles from "../../styles/AddLabelForm.module.css";
 
 const AddLabelsForm = () => {
+  const [selectedIndices, setSelectedIndices] = useState([]);
+
+  const buttons = [
+    "Button 1",
+    "Button 2",
+    "Button 3",
+    "Button 4",
+    "Button 5",
+    "Button 6",
+    "Button 7",
+    "Button 8",
+  ];
+
+  const toggleSelection = (index) => {
+    setSelectedIndices((prevSelected) => {
+      if (prevSelected.includes(index)) {
+        // Deselect if already selected
+        return prevSelected.filter((i) => i !== index);
+      } else {
+        // Select if not already selected
+        return [...prevSelected, index];
+      }
+    });
+  };
 
   return (
     <Form className={styles.AddLabelForm}>
       <Container className="text-white">
-        <Row className="d-flex justify-content-evenly">
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            1 of 2
-          </Col>
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            2 of 2
-          </Col>
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            2 of 2
-          </Col>
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            2 of 2
-          </Col>
-        </Row>
-        <Row className="d-flex justify-content-evenly">
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            1 of 3
-          </Col>
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            2 of 3
-          </Col>
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            3 of 3
-          </Col>
-          <Col
-            xs={5}
-            lg={2}
-            className={`align-content-center offset-xs-1 offset-lg-1 ${styles.Label}`}
-          >
-            2 of 2
-          </Col>
+        <Row gap={3} className="d-flex justify-content-evenly">
+          {buttons.map((label, index) => {
+            const isSelected = selectedIndices.includes(index);
+
+            const buttonStyle = isSelected
+              ? { backgroundColor: "#1f1f1f" }
+              : undefined;
+
+            return (
+              <Col
+                key={index}
+                xs={{ span: 5, offset: 1 }}
+                lg={{ span: 4 }}
+                className={`mx-1 align-content-center ${styles.Label}`}
+                style={buttonStyle}
+                onClick={() => toggleSelection(index)}
+              >
+                {label}
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </Form>
