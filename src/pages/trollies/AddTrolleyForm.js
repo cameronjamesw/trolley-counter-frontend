c01d1d6793678af6c7cc0f2e9b0ab5c73636819a
Cameron <cameron_2000@live.co.uk>
2025-06-14 19:37:50 +0100

    ensure formdata is correct through logs

--- src/pages/trollies/AddTrolleyForm.js
+++ src/pages/trollies/AddTrolleyForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
 import { Button, Container, Form } from "react-bootstrap";
-import styles from "../../styles/AddTrolleyForm.module.css"
+import styles from "../../styles/AddTrolleyForm.module.css";
 import AddLabelsForm from "./AddLabelsForm";
 import { useTrolleyForm } from "../../contexts/TrolleyFormContext";
 
@@ -14,6 +14,7 @@ const AddTrolleyForm = () => {
     updateField,
     toggleInUse,
     updateToteCount,
+    formData
   } = useTrolleyForm();
 
   if (currentUser === undefined) {
@@ -43,60 +44,64 @@ const AddTrolleyForm = () => {
 
   return (
     <div>
-    <Form onSubmit={handleSubmit} className={styles.AddTrolleyForm}>
-      <h1 className="text-white my-4">Add Trolley</h1>
-      <Container className="row">
-        <p className="fst-italic text-body-secondary text-left text-start">
-          Please specify the amount of totes this trolley has, and check if the
-          trolley is in use.
-        </p>
-        <Form.Group className="col-10" controlId="tote-count">
-          <Form.Label className="d-none">Tote Count</Form.Label>
-          <Form.Select
-            name="totes_count"
-            value={totes_count}
-            onChange={handleSelectChange}
-          >
-            <option value="">-- Select Count --</option>
-            <option value="1">8 Totes</option>
-            <option value="2">10 Totes</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="col align-self-center" controlId="in-use">
-          <Form.Label className="d-none">In Use?</Form.Label>
-          <Form.Check
-            type="switch"
-            id="custom-switch"
-            checked={in_use}
-            onChange={handleSwitchChange}
-          />
-        </Form.Group>
-      </Container>
+      <Form onSubmit={handleSubmit}>
+        <Container className={styles.AddTrolleyForm}>
+          <h1 className="text-white my-4">Add Trolley</h1>
+          <Container className="row">
+            <p className="fst-italic text-body-secondary text-left text-start">
+              Please specify the amount of totes this trolley has, and check if
+              the trolley is in use.
+            </p>
+            <Form.Group className="col-10" controlId="tote-count">
+              <Form.Label className="d-none">Tote Count</Form.Label>
+              <Form.Select
+                name="totes_count"
+                value={totes_count}
+                onChange={handleSelectChange}
+              >
+                <option value="">-- Select Count --</option>
+                <option value="1">8 Totes</option>
+                <option value="2">10 Totes</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="col align-self-center" controlId="in-use">
+              <Form.Label className="d-none">In Use?</Form.Label>
+              <Form.Check
+                type="switch"
+                id="custom-switch"
+                checked={in_use}
+                onChange={handleSwitchChange}
+              />
+            </Form.Group>
+          </Container>
 
-<Container className="row">
-      <Form.Group className="mt-4 col" controlId="notes">
-        <p className="fst-italic text-body-secondary text-left text-start">
-          Add any notes to this trolley.
-          For example, "the left break is faulty"
-        </p>
-        <Form.Label className="d-none">Password</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={4}
-          placeholder="Enter any notes here..."
-          name="notes"
-          value={notes}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      </Container>
+          <Container className="row">
+            <Form.Group className="mt-4 col" controlId="notes">
+              <p className="fst-italic text-body-secondary text-left text-start">
+                Add any notes to this trolley. For example, "the left break is
+                faulty"
+              </p>
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                placeholder="Enter any notes here..."
+                name="notes"
+                value={notes}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Container>
+        </Container>
 
-      <Button variant="success" className="mt-4" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <AddLabelsForm totes_count={totes_count} />
 
-    <AddLabelsForm totes_count={totes_count} />
+        <Container className={`${styles.AddTrolleyForm} mb-4`}>
+          <Button variant="success" type="submit">
+            Create Trolley
+          </Button>
+        </Container>
+      </Form>
     </div>
   );
 };
