43cc669791a03039a6f8395e1b8003e9bbaf5e14
Cameron <cameron_2000@live.co.uk>
2025-06-17 12:25:04 +0100

    create axiosReq interceptor, log trolley details to user

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,14 @@ const baseURL = "https://trolley-counter-backend-3f175e45a111.herokuapp.com";
 
 export const axiosReq = axios.create({ baseURL, withCredentials: true });
 export const axiosRes = axios.create({ baseURL, withCredentials: true });
+
+axiosReq.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("access");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
--- src/pages/trollies/TrolleyDetail.js
+++ src/pages/trollies/TrolleyDetail.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults';
 
 const TrolleyDetail = () => {
+    const [trolley, setTrolley] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchTrolley = async () => {
+            try {
+                const { data } = await axiosReq.get(`/api/trolleys/${id}/`);
+                console.log(data);
+                setTrolley(data);
+            } catch (err) {
+                console.log(err?.response.data);
+            };
+        };
+
+        fetchTrolley();
+    }, [])
   return (
     <Container>
         <h1>
             Trolley Detail Window!
         </h1>
+        <p>Trolley Number: {trolley.id}</p>
+        <p>Creator: {trolley.creator}</p>
+        <p>Totes: {trolley.totes_count}</p>
+        <p>Front Labels Count: {trolley.front_label_count}</p>
+        <p>Back Labels Count: {trolley.back_label_count}</p>
+        <p>Missing Front Labels: {trolley.missing_front_labels}</p>
     </Container>
   )
 }
