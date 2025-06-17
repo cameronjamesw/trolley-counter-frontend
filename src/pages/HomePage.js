405ef8d0ddfa7aaf29b08f5bc032db44c74483cc
Cameron <cameron_2000@live.co.uk>
2025-06-17 10:46:02 +0100

    add conditional rendering to trolley components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Container, Row } from 'react-bootstrap'
-import TrolleyIndex from '../components/TrolleyIndex'
-import TrolleyInfo from '../components/TrolleyInfo'
-import PinnedTrollies from '../components/PinnedTrollies'
-import RecentTrollies from '../components/RecentTrollies'
-import styles from "../styles/HomePage.module.css"
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import TrolleyIndex from "../components/TrolleyIndex";
+import TrolleyInfo from "../components/TrolleyInfo";
+import PinnedTrollies from "../components/PinnedTrollies";
+import RecentTrollies from "../components/RecentTrollies";
+import styles from "../styles/HomePage.module.css";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const HomePage = () => {
+  const currentUser = useCurrentUser();
   return (
     <Container>
-        < TrolleyIndex />
-        < TrolleyInfo />
-        <Row className='d-flex justify-content-between g-0'>
-          < PinnedTrollies />
-          < RecentTrollies />
+      <TrolleyIndex />
+      <TrolleyInfo />
+      {currentUser && (
+        <Row className="d-flex justify-content-between g-0">
+          <PinnedTrollies />
+          <RecentTrollies />
         </Row>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
