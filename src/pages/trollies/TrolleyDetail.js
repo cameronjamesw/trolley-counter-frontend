79cb131a0e32ff9ccf84d5bf9121962b608756b3
Cameron <cameron_2000@live.co.uk>
2025-06-18 08:55:23 +0100

    add skeleton to TrolleyDetail, create corresponding css file

--- src/pages/trollies/TrolleyDetail.js
+++ src/pages/trollies/TrolleyDetail.js
@@ -1,38 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
-import { axiosReq } from '../../api/axiosDefaults';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import styles from "../../styles/TrolleyDetail.module.css";
 
 const TrolleyDetail = () => {
-    const [trolley, setTrolley] = useState({});
-    const { id } = useParams();
+  const [trolley, setTrolley] = useState({});
+  const { id } = useParams();
 
-    useEffect(() => {
-        const fetchTrolley = async () => {
-            try {
-                const { data } = await axiosReq.get(`/api/trolleys/${id}/`);
-                console.log(data);
-                setTrolley(data);
-            } catch (err) {
-                console.log(err?.response.data);
-            };
-        };
+  useEffect(() => {
+    const fetchTrolley = async () => {
+      try {
+        const { data } = await axiosReq.get(`/api/trolleys/${id}/`);
+        console.log(data);
+        setTrolley(data);
+      } catch (err) {
+        console.log(err?.response.data);
+      }
+    };
 
-        fetchTrolley();
-    }, [])
+    fetchTrolley();
+  }, []);
   return (
     <Container>
-        <h1>
-            Trolley Detail Window!
-        </h1>
-        <p>Trolley Number: {trolley.id}</p>
-        <p>Creator: {trolley.creator}</p>
-        <p>Totes: {trolley.totes_count}</p>
-        <p>Front Labels Count: {trolley.front_label_count}</p>
-        <p>Back Labels Count: {trolley.back_label_count}</p>
-        <p>Missing Front Labels: {trolley.missing_front_labels}</p>
+      <Row className="mt-5">
+        <Col xs={12} md={{ span: 5 }} className={`${styles.Div}`}>
+          Trolley Info Window
+        </Col>
+        <Col xs={12} md={{ span: 5, offset: 2 }}>
+          <Row className={`text-center mb-3 ${styles.Div}`}>
+            <Col>Missing Front Labels</Col>
+          </Row>
+          <Row className={`text-center mt-3 ${styles.Div}`}>
+            <Col>Missing Back Labels</Col>
+          </Row>
+        </Col>
+      </Row>
+      <h1>Trolley Detail Window!</h1>
+      <p>Trolley Number: {trolley.id}</p>
+      <p>Creator: {trolley.creator}</p>
+      <p>Totes: {trolley.totes_count}</p>
+      <p>Front Labels Count: {trolley.front_label_count}</p>
+      <p>Back Labels Count: {trolley.back_label_count}</p>
+      <p>Missing Front Labels: {trolley.missing_front_labels}</p>
     </Container>
-  )
-}
+  );
+};
 
-export default TrolleyDetail
+export default TrolleyDetail;
