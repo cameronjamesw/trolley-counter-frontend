f12e4cf633932092b04be91f38ba787c1febeb7d
Cameron <cameron_2000@live.co.uk>
2025-09-09 10:30:08 +0100

    add comments to utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,10 @@
 import { jwtDecode } from "jwt-decode";
 
+/**
+ * This function sets the token timestamp using the
+ * JSON web token for session authentication.
+ * @param {*} access 
+ */
 export const setTokenTimestamp = (access) => {
   try {
     const decoded = jwtDecode(access);
@@ -9,10 +14,19 @@ export const setTokenTimestamp = (access) => {
   }
 };
 
+/**
+ * This cleanup function removes the token timestamp
+ * upon the user logging out.
+ */
 export const removeTokenTimestamp = () => {
   localStorage.removeItem("tokenTimestamp");
 };
 
+/**
+ * This function determines whether the token
+ * needs to be refreshed based on the TTL left
+ * on the timestamp. 
+ */
 export const shouldRefreshToken = () => {
   const timestamp = localStorage.getItem("tokenTimestamp");
   if (!timestamp) return false;
@@ -22,6 +36,19 @@ export const shouldRefreshToken = () => {
   return now >= timestamp - buffer;
 };
 
+/**
+ * This function takes an array of labels and checks them
+ * against the localStorage. If the label is indexed, the
+ * function removes it from localStorage, otherwise it will
+ * set the key and value as the index. 
+ * 
+ * @param {Array} prev - contains the previous labels for the function to interpet 
+ * @param {number} index - contains the index of the labels parsed
+ * @param {'front' | 'back'} side - refers to which side of the trolley the labels are on
+ * @param {boolean} submit - if the function is called within a from submittion, value is true
+ * @param {boolean} unmount - if function is called when unmounting, value is true
+ * @returns an updated localStorage reflecting the labels passsed.
+ */
 export const handleLocalStorage = (
   prev,
   index,
@@ -50,6 +77,13 @@ export const handleLocalStorage = (
   }
 };
 
+/**
+ * This function takes the labels from the fetched trolley
+ * and adds them to the localStorage based on the
+ * 'checked' attribute of the labels. Then, the labels will
+ * appear as selected when the component mounts to the user.
+ * @param {Array} labels - an array of the recently fetched labels
+ */
 export const handleLabelsMount = (labels) => {
   const { front, back } = labels;
   front.forEach((label, idx) => {
@@ -64,6 +98,12 @@ export const handleLabelsMount = (labels) => {
   });
 };
 
+/**
+ * This function takes the labels from the fetched trolley
+ * as a parameter, and then removes the indexes from the
+ * localStorage upon the component unmounting.
+ * @param {Array} labels - an array of the mounted labels
+ */
 export const handleLabelUnmount = (labels) => {
   const { front, back } = labels;
   front.forEach((label, idx) => {
