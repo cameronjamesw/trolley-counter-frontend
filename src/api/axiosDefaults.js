cb2f625353ba7abe9fc8f1dfe01a650dd92baf3c
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-15 18:04:27 -0400

    update project for render deployment

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://trolley-counter-backend-3f175e45a111.herokuapp.com";
+const baseURL = "https://trolley-counter-backend.onrender.com/";
 
 export const axiosReq = axios.create({ baseURL, withCredentials: true });
 export const axiosRes = axios.create({ baseURL, withCredentials: true });
